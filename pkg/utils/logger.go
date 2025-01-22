b079c767a9ec6117393f4d46f1582124c03881a3
Rick Ryan <rick.ryan2@outlook.com>
2025-01-22 23:47:06 +0000

    fix(utils): logger v-level

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -7,11 +7,12 @@ import (
 	"github.com/go-logr/logr"
 )
 
-func NewLogger() *logr.Logger {
+func NewLogger(level int) *logr.Logger {
 	logger := logr.FromSlogHandler(slog.NewJSONHandler(
 		os.Stdout,
 		&slog.HandlerOptions{
 			AddSource: true,
+			Level: slog.Level(0-level),
 		},
 	))
 	return &logger
