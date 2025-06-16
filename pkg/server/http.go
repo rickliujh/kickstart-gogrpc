1aea5ffe6f0f4a1efd40e4da70234fb6b84b6964
Rick Liu <rick.ryan2@outlook.com>
2025-06-16 21:39:17 +0000

    feat: http server support config

--- pkg/server/http.go
+++ pkg/server/http.go
@@ -1,17 +1,23 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func HTTPServer() {
+var (
+	// set at build time
+	version = "v0.0.1-default"
+)
+
+func HTTPServer(addr, name, env, dbConnStr string) {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Server: %s, Version: %s, Environment: %s", name, version, env)))
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(addr, r)
 }
